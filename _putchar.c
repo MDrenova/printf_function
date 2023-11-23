a406d6230231885f71a4a64c1c9fe72b7bfd7f70
MDrenova <mateodrenova@gmail.com>
2023-11-23 13:15:20 -0800

    Update return

--- _putchar.c
+++ _putchar.c
@@ -1,4 +1,6 @@
+#include <unistd.h>
 #include "main.h"
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -6,56 +8,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _printf(const char *format, ...)
+int _putchar(char c)
 {
-    format_t formats[] = {
-        {"c", print_char},
-        {"s", print_string},
-        {"d", print_int},
-        {"i", print_int},
-        {NULL, NULL}
-    };
-
-    va_list args;
-    int i = 0, j = 0, len = 0;
-
-    va_start(args, format);
-
-    while (format && format[i])
-    {
-        if (format[i] == '%' && format[i+1] != '%')
-        {
-            i++;
-            while (formats[j].spec != NULL)
-            {
-                if (format[i] == *(formats[j].spec))
-                {
-                    len += formats[j].f(args);
-                    break;
-                }
-                j++;
-            }
-            if (formats[j].spec == NULL)
-            {
-                putchar('%');
-                putchar(format[i]);
-                len += 2;
-            }
-        }
-        else if (format[i] == '%' && format[i+1] == '%')
-        {
-            putchar('%');
-            len++;
-        }
-        else
-        {
-            putchar(format[i]);
-            len++;
-        }
-        i++;
-    }
-
-    va_end(args);
-
-    return (len);
+	return (write(1, &c, 1));
 }
