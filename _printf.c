373d71de9635a9d13ac5959df3729063f2402980
MDrenova <mateodrenova@gmail.com>
2023-11-23 13:39:41 -0800

    Update code to betty style

--- _printf.c
+++ _printf.c
@@ -7,51 +7,50 @@
  */
 int _printf(const char *format, ...)
 {
-    format_t formats[] = {
-        {"c", print_char},
-        {"s", print_string},
-        {"d", print_int},
-        {"i", print_int},
-        {NULL, NULL}
-    };
+	format_t formats[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"d", print_int},
+		{"i", print_int},
+		{NULL, NULL}
+	};
 
-    va_list args;
-    int i = 0, j = 0, len = 0;
+	va_list args;
+	int i = 0, j = 0, len = 0;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format && format[i])
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            j = 0;
-            while (formats[j].spec != NULL)
-            {
-                if (format[i] == *(formats[j].spec))
-                {
-                    len += formats[j].f(args);
-                    break;
-                }
-                j++;
-            }
-            if (formats[j].spec == NULL)
-            {
-                putchar('%');
-                putchar(format[i]);
-                len += 2;
-            }
-        }
-        else
-        {
-            putchar(format[i]);
-            len++;
-        }
-        i++;
-    }
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			j = 0;
+			while (formats[j].spec != NULL)
+			{
+				if (format[i] == *(formats[j].spec))
+				{
+					len += formats[j].f(args);
+					break;
+				}
+				j++;
+			}
+			if (formats[j].spec == NULL)
+			{
+				putchar('%');
+				putchar(format[i]);
+				len += 2;
+			}
+		}
+		else
+		{
+			putchar(format[i]);
+			len++;
+		}
+		i++;
+	}
 
-    va_end(args);
+	va_end(args);
 
-    return (len);
+	return (len);
 }
-
